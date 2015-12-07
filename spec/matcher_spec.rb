37d62248d3b967752e3a6d5f222d02d2643886ef
Sam Russell <sam.russell@powershop.co.nz>
2015-12-07 14:47:03 +1300

    happy with +

--- spec/matcher_spec.rb
+++ spec/matcher_spec.rb
@@ -5,43 +5,41 @@ describe Matcher do
   describe '#matches?' do
     context 'with letter' do
       let(:matcher) { Matcher.new('a') }
-      let(:char1) { 'a' }
-      let(:char2) { 'b' }
 
       it 'matches "a"' do
-        expect(matcher.matches?(char1)).to be true
+        expect(matcher.matches?('a')).to be true
       end
 
       it 'does not match "b"' do
-        expect(matcher.matches?(char2)).to be false
+        expect(matcher.matches?('b')).to be false
       end
     end
 
     context 'with dot' do
       let(:matcher) { Matcher.new('.') }
-      let(:char1) { 'a' }
-      let(:char2) { 'b' }
+      let(:'a') { 'a' }
+      let(:'b') { 'b' }
 
       it 'matches "a"' do
-        expect(matcher.matches?(char1)).to be true
+        expect(matcher.matches?('a')).to be true
       end
 
       it 'matches "b"' do
-        expect(matcher.matches?(char2)).to be true
+        expect(matcher.matches?('b')).to be true
       end
     end
 
     context 'with plus' do
       let(:matcher) { Matcher.new('a', '+') }
-      let(:char1) { 'a' }
-      let(:char2) { 'b' }
+      let(:'a') { 'a' }
+      let(:'b') { 'b' }
 
       it 'matches "a"' do
-        expect(matcher.matches?(char1)).to be true
+        expect(matcher.matches?('a')).to be true
       end
 
       it 'does not match "b"' do
-        expect(matcher.matches?(char2)).to be false
+        expect(matcher.matches?('b')).to be false
       end
     end
   end
--- spec/regex_spec.rb
+++ spec/regex_spec.rb
@@ -50,5 +50,22 @@ describe Regex do
         expect(Regex.new(pattern_string).matches?('bar')).to be false
       end
     end
+
+    context 'pattern_string /f.+/' do
+      let(:pattern_string) { '/f.+/' }
+
+      it 'matches foo' do
+        expect(Regex.new(pattern_string).matches?('foo')).to be true
+      end
+
+      it 'matches far' do
+        expect(Regex.new(pattern_string).matches?('far')).to be true
+      end
+
+      it 'does not match bar' do
+        expect(Regex.new(pattern_string).matches?('bar')).to be false
+      end
+    end
+
   end
 end
